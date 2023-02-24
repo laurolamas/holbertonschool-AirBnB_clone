0a1012630b9f6f7be63a8bc97422f576828b2867
Jeronimo Lussich <jerolussich@hotmail.com>
2023-02-24 15:56:06 -0300

    test state added

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import unittest
+from models.state import State
+
+
+class TestState(unittest.TestCase):
+    """test user model"""
+
+    def test_state(self):
+        """test state"""
+        state = State()
+        self.assertTrue(type(state) is str)
